01a07e5f3b72c44476e17adb06035d2591cc25b9
JonathanCedrim <jonathan.cedrim.souza@gmail.com>
2018-01-11 18:41:59 -0200

    Ideal: Crud

---
+++ src/main/java/br/com/amora/xavier/model/Ideal.java
@@ -0,0 +1,58 @@
+package br.com.amora.xavier.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@EqualsAndHashCode
+public class Ideal implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    private long id;
+
+    @Getter
+    @Setter
+    private long codigo;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    private Vendedor vendedor;
+
+    @Getter
+    @Setter
+    private Date dataLancamento;
+
+    @Getter
+    @Setter
+    private Date dataInicial;
+
+    @Getter
+    @Setter
+    private Date dataFinal;
+
+    @Getter
+    @Setter
+    private int totalRecebido;
+
+    @Getter
+    @Setter
+    private int ideal;
+
+    @Getter
+    @Setter
+    private long sobra;
+
+    @Getter
+    @Setter
+    private String responsavel;
+}
